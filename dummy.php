d2688b714cc8d0b715d135423cd554ec41ac9fc3
SabbirMMS <120826117+SabbirMMS@users.noreply.github.com>
2023-11-23 12:24:20 +0600

    Update dummy.php

--- dummy.php
+++ dummy.php
@@ -1,10 +1,24 @@
 <?php
-
-// Check if the key parameter is set and not null
-if (isset($_GET['key']) && $_GET['key'] !== null) {
-    echo "Success! The parameter key has a non-null value.";
-} else {
-    echo "Invalid parameter. The key parameter is missing or has a null value.";
+// Function to generate a random table
+function generateRandomTable($rows, $cols) {
+    echo '<table border="1">';
+    for ($i = 0; $i < $rows; $i++) {
+        echo '<tr>';
+        for ($j = 0; $j < $cols; $j++) {
+            // Generate a random number between 1 and 100 for each cell
+            $randomValue = rand(1, 100);
+            echo '<td>' . $randomValue . '</td>';
+        }
+        echo '</tr>';
+    }
+    echo '</table>';
 }
 
+// Set the number of rows and columns
+$rows = 5;
+$cols = 5;
+
+// Call the function to generate the random table
+generateRandomTable($rows, $cols);
 ?>
+
